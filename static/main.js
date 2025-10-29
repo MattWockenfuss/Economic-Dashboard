964522767f64f0aacebeb070af38bb4aedea77a8
Matt Wockenfuss <223121734+MattWockenfuss@users.noreply.github.com>
2025-10-29 15:21:36 -0400

    update

--- static/main.js
+++ static/main.js
@@ -1,6 +1,10 @@
-const map = new maplibregl.Map({
-    container: "map",
-    style: "https://demotiles.maplibre.org/style.json",
-    center: [0, 0],
-    zoom: 2
-});
+var data = [{
+  type: "choroplethmap", 
+  locations: ["NY", "MA", "VT", "TX", "PA"], 
+  z: [-50, -10, -20, -90, -40],
+  geojson: "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/us-states.json"
+}];
+
+var layout = {map: {center: {lon: -74, lat: 43}, zoom: 3.5}};
+
+Plotly.newPlot('map', data, layout);
