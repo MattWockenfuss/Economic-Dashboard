8bb9b7111b2c9621cf979699e972c5de85d99a21
redmattr <mjrst268@mail.rmu.edu>
2025-10-30 15:25:31 -0400

    Placed Choropleth map generation into its own function, edited bounds

--- static/main.js
+++ static/main.js
@@ -1,29 +1,19 @@
 //    https://plotly.com/javascript/reference/choroplethmap/
 
-
+/*
 var data = [{
   type: "choroplethmap", 
-  locations: ["NY", "MA", "VT", "TX", "PA"], 
-  z: [-50, -10, -20, -90, -40],
+  locations: ["NY", "MA", "VT", "TX", "PA", "VT", "MA", "MN", "HI", "AK"], 
+  z: [-50, -10, -20, -90, -40, -15, -100, -200, -30, -20],
   geojson: "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/us-states.json"
 }];
-
-/*
-const bounds = [
-    [-180, 10], //Southwest coordinates
-    [-40, 90] //Northeast coordinates
-];
-*/
-
-
-
 
 const layout = {
   map: {
     style: "carto-darkmatter-nolabels", // any MapLibre style
     center: { lat: 38.8283, lon: -98.5795 },
     zoom: 4.5,
-    bounds: { east: -40, north: 90, south: 10, west: -180},
+    bounds: { east: -60, north: 72, south: 17, west: -172},
     margin: {t:0,r:0,b:0,l:0}
   }
 };
@@ -35,23 +25,6 @@ var config = {
     displaylogo: false        //displays the logo on modebar
 };
 
-
-
-
-
-// let map2 = {
-//     container: 'mapplot',
-//     center: [-98.5795, 38.8283],
-//     zoom: 5,
-//     maxBounds: bounds, //Set to max
-//     canvasContextAttributes: {antialias: true}
-// };
-
-
-
-
-
-
 Plotly.newPlot('mapplot', data, layout, config);
 
 
@@ -59,5 +32,70 @@ Plotly.newPlot('mapplot', data, layout, config);
 map.on('mousemove', (e) => {
       console.log(`lon: ${e.lngLat.lng.toFixed(4)}, lat: ${e.lngLat.lat.toFixed(4)}`);
 });
+*/
 
+function GenChoropleth() {
+  //If data is for choropleth
 
+  var data = [{
+    type: "choroplethmap", 
+    locations: ["NY", "MA", "VT", "TX", "PA", "VT", "MA", "MN", "HI", "AK"], 
+    z: [-50, -10, -20, -90, -40, -15, -100, -200, -30, -20],
+    geojson: "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/us-states.json"
+  }];
+
+  const layout = {
+    map: {
+      style: "carto-darkmatter-nolabels", // any MapLibre style
+      center: { lat: 38.8283, lon: -98.5795 },
+      zoom: 4.5,
+      bounds: { east: -60, north: 72, south: 17, west: -172},
+      margin: {t:0,r:0,b:0,l:0}
+    }
+  };
+
+  var config = {
+    scrollZoom: true,      //users cant scroll
+    editable: false,        //users cant edit the data
+    displayModeBar: false,   //the command bar, options, etc...
+    displaylogo: false        //displays the logo on modebar
+  };
+
+  Plotly.newPlot('mapplot', data, layout, config);
+
+  return true;
+}
+
+function GenDensity() {
+  //If data is for density
+
+  var data = [{
+    type: "densitymap", 
+    lon: [-98, -105, -110], lat: [35, 30, 32], z: [1, 3, 5],
+    radius: 100, colorbar: {y: 1, yanchor: 'top', len: 0.45},
+    geojson: "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/us-states.json"
+  }];
+
+  const layout = {
+    map: {
+      style: "carto-darkmatter-nolabels", // any MapLibre style
+      center: { lat: 38.8283, lon: -98.5795 },
+      zoom: 4.5,
+      bounds: { east: -60, north: 72, south: 17, west: -172},
+      margin: {t:0,r:0,b:0,l:0}
+    }
+  };
+
+  var config = {
+    scrollZoom: true,      //users cant scroll
+    editable: false,        //users cant edit the data
+    displayModeBar: false,   //the command bar, options, etc...
+    displaylogo: false        //displays the logo on modebar
+  };
+
+  Plotly.newPlot('mapplot', data, layout, config);
+
+  return true;
+}
+
+GenChoropleth();
